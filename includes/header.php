26cba04260cf3bb6cfd184838c22b07344889be4
Schadrack2544 <nzayinaschad250@gmail.com>
2019-09-18 09:07:31 +0200

    Adding jquery in project

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="public/bootstrap4/css/bootstrap.min.css">
+    <script src="public/js/jquery.min.js"></script>
     <script src="public/bootstrap4/js/bootstrap.min.js"></script>
     <title></title>
 </head>
