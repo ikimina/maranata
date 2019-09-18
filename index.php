81c6ac13bfb637cb10972c9f1b4a96e5d46eb499
Schadrack2544 <nzayinaschad250@gmail.com>
2019-09-18 10:39:55 +0200

    Changing something

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "includes/header.php";
 <div class="wrapper">
         <div class="row all">
         <div class="head">
-            <h3>Welcome to ikimina-maranata </h3>
+            <h3>Welcome to Ikimina-Maranata Schadrack edition </h3>
         </div>
             <div class="col-md-5 use">
            
