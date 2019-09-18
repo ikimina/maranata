a326b4107d234aec2da5e6f78b40a81167527ceb
Schadrack2544 <nzayinaschad250@gmail.com>
2019-09-18 09:00:32 +0200

    Adding new files again

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+    <div>&copy; <script>
+        var date=new Date();
+    document.write(date.getFullYear());</script><a href="index.php">Ikimina</a> </div>
+</div>
+</body>
+</html>
