f42048a1198af04ea862f083dcee081eba596a54
Schadrack2544 <nzayinaschad250@gmail.com>
2019-09-18 05:46:43 +0200

    Adding more files

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Ikimina | Home</title>
-</head>
-<body>
-<center><h2>WELCOME TO MARANATA</h2></center>
-<center>Schadrack is welcomed to this project</center>
-<center><h2> David WELCOME TO MARANATA</h2></center>
+<?php
+include "includes/header.php";
+?>
+<div class="wrapper">
+        <div class="row">
+        <h3>Welcome to ikimina-maranata </h3>
+            <div class="head"></div>
+            <div class="col-md-5 use">
+                <form action="login.php" method="POST">
+                    <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Login Here!</h1>
+                    </div>
+                    <div class="form-group">
+                    <input type="text" name="username" class="form-control form-control-user" placeholder="Username" required>
+                    </div>
+                    <div class="form-group">
+                    <input type="password" name="password" class="form-control form-control-user" placeholder="Password" required>
+                    </div>
+                    <button type="submit" name="login" class="btn btn-primary btn-user btn-block"> Login </button>
+                    <hr>
+                </form>
+            </div>
+          
+        </div>
+</div>
+</div>
 
-<center><h2>Eric Changes WELCOME TO MARANATA</h2></center>
-</body>
-</html>
+<?php
+include "includes/footer.php";
+?>
